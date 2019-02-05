9228949a05acdf37782b3366561cdff29e3dd8ee
T W Webb <TW.Webb@ilg.com>
2019-02-05 10:42:54 -0500

    Signed-off-by: T W Webb <TW.Webb@ilg.com>

---
+++ zillowZestimateScrape.py
@@ -0,0 +1,9 @@
+
+import pandas as pd
+import os
+
+#os.chdir(r"C:\Users\twebb\Documents\GitHub\zillow")
+props = pd.read_csv('properties-32803.csv')
+print(props.head())
+
+zpid = "X1-ZWz1850vlsqpsb_59mre"
