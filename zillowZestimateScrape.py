7e4b7702521a841e6fe4c5356fd9b7ff362c50ef
T W Webb <TW.Webb@ilg.com>
2019-02-05 15:04:01 -0500

    Signed-off-by: T W Webb <TW.Webb@ilg.com>

--- zillowZestimateScrape.py
+++ zillowZestimateScrape.py
@@ -1,9 +1,49 @@
 
 import pandas as pd
-import os
+import zillow
 
-#os.chdir(r"C:\Users\twebb\Documents\GitHub\zillow")
+
 props = pd.read_csv('properties-32803.csv')
 print(props.head())
 
-zpid = "X1-ZWz1850vlsqpsb_59mre"
+
+import config
+
+key = config.zpid
+
+api = zillow.ValuationApi()
+
+address = "3400 Pacific Ave., Marina Del Rey, CA"
+postal_code = "90292"
+
+#data = api.GetSearchResults(key, address, postal_code)
+
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+#pp.pprint(data.get_dict())
+#print(data.get_dict()["zestimate"])
+
+address = str(props["address"][0])+", Orlando, FL"
+
+#data = api.GetSearchResults(key, address, postal_code)
+#print(data.get_dict()["zestimate"]['amount'])
+
+zestimate= []
+zestimateDiff = []
+
+postal_code = "32803"
+for i in range(0,len(props['address'])):
+    try:
+        address = str(props["address"][i])+", Orlando, FL"
+        data = api.GetSearchResults(key, address, postal_code)
+        zestimate.append(data.get_dict()["zestimate"]['amount'])
+        zestimateDiff.append(data.get_dict()["zestimate"]['amount'] - (float(props['price'][i].strip('$').replace(',',''))))
+    except:
+        zestimate.append(0)
+        zestimateDiff.append(0)
+print(len(props['address']))
+
+props['zestimate'] = zestimate
+props['zestimateDiff'] = zestimateDiff
+
+props.to_excel('Zillow Zestimate Data.xlsx')
